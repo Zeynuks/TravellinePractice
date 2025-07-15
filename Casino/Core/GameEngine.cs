02b6d9e655ff9202a3000315ea5c4010ee758a29
Zeynuks <leznin2000@ya.ru>
2025-07-16 01:38:41 +0300

    Заменил Random на Random.Shared

--- Casino/Core/GameEngine.cs
+++ Casino/Core/GameEngine.cs
@@ -4,11 +4,10 @@ namespace Casino.Core
     {
         private const int _winThreshold = 18;
         private const int _maxRoll = 20;
-        private readonly Random _rand = new();
 
         public BetResult PlayRound( Money bet, int mult )
         {
-            int rolled = _rand.Next( 1, _maxRoll + 1 );
+            int rolled = Random.Shared.Next(1, _maxRoll + 1);
             if ( rolled < _winThreshold )
             {
                 return new BetResult( false, bet );

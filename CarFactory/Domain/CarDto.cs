02877850e5afdcb1551e613cb85a6018e6af4e80
Zeynuks <leznin2000@ya.ru>
2025-08-04 23:56:34 +0300

    Обновил Dto класс

--- CarFactory/Domain/CarDto.cs
+++ CarFactory/Domain/CarDto.cs
@@ -7,19 +7,10 @@ namespace CarFactory.Domain
 {
     public class CarDto
     {
-        public string Number { get; set; }
-        public Color Color { get; set; }
-        public BodyType Body { get; set; }
-        public EngineType Engine { get; set; }
-        public TransmissionType Transmission { get; set; }
-
-        public CarDto()
-        {
-            Number = "A000AA";
-            Color = Color.Black;
-            Body = BodyType.Coupe;
-            Engine = EngineType.Petrol;
-            Transmission = TransmissionType.Automatic;
-        }
+        public string Number { get; set; } = "A000AA";
+        public Color Color { get; set; } = Color.Black;
+        public BodyType Body { get; set; } = BodyType.Coupe;
+        public EngineType Engine { get; set; } = EngineType.Petrol;
+        public TransmissionType Transmission { get; set; } = TransmissionType.Automatic;
     }
 }
